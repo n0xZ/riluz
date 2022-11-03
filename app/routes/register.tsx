86b21f368413c91dded99244c9e335f071eaaf8c
n0xZ <o.gonzalo@hotmail.com>
2022-11-02 23:09:06 -0300

    setup register auth logic on action and pending ui

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,4 +1,16 @@
+import type { ActionArgs, LoaderArgs } from '@remix-run/node'
+import { json } from '@remix-run/node'
+import { redirect } from '@remix-run/node'
+import { Form, Link, useActionData, useTransition } from '@remix-run/react'
+import {
+	errorMessagesForSchema,
+	inputFromForm,
+	makeDomainFunction,
+} from 'domain-functions'
 import { z } from 'zod'
+import { FormField } from '~/components/form/FormField'
+import {  register } from '~/utils/auth.server'
+import { createUserSession, getSession } from '~/utils/session.server'
 
 export const registerSchema = z.object({
 	username: z.string().min(4, { message: 'Campo requerido' }),
@@ -10,6 +22,72 @@ export const registerSchema = z.object({
 	password: z.string().min(4, { message: 'Campo requerido' }),
 })
 
+const registerFunc = makeDomainFunction(registerSchema)(
+	async ({ email, password,username }) => {
+		const registerResult = await register({ email, password,username })
+		if (!registerResult) throw new Error('Credenciales incorrectas')
+		return registerResult
+	}
+)
+export const action = async ({ request }: ActionArgs) => {
+	const result = await registerFunc(await inputFromForm(request))
+	if (result.success) return createUserSession(result.data.id)
+
+	const inputErrors = errorMessagesForSchema(result.inputErrors, registerSchema)
+	return json(
+		{
+			email: inputErrors.email,
+			password: inputErrors.password,
+			externalErrors: result.errors,
+		},
+		{ status: result.success ? 200 : 400 }
+	)
+}
+export const loader = async ({ request }: LoaderArgs) => {
+	const session = await getSession(request)
+
+	if (session.get('userId')) return redirect('/home')
+	return null
+}
 export default function Register() {
-	return <div>Register</div>
+	const actionData = useActionData<typeof action>()
+	const transition = useTransition()
+	const isSubmitting = transition.state === 'submitting'
+	return (
+		<Form
+			ref={(e) => isSubmitting && e?.reset()}
+			method="post"
+			className="grid card-body place-items-center"
+		>
+			<FormField
+				label="Correo electrónico"
+				name="email"
+				type="email"
+				errors={actionData && actionData.email && actionData?.email[0]}
+				data-test="email-input"
+			/>
+			<FormField
+				label="Contraseña"
+				name="password"
+				type="password"
+				errors={actionData && actionData.password && actionData?.password[0]}
+				data-test="password-input"
+			/>
+
+			<button
+				type="submit"
+				className="btn btn-primary "
+				disabled={isSubmitting}
+				name="submit-login"
+			>
+				{!isSubmitting ? 'Crear cuenta' : 'Creando...'}
+			</button>
+			<Link to="/login">Ya tengo una cuenta</Link>
+			<span className="text-red-500 h-9 ">
+				{actionData &&
+					actionData.externalErrors &&
+					actionData.externalErrors[0].message}
+			</span>
+		</Form>
+	)
 }
