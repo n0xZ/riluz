4683d8d7d5bb7e847682953e551528c2009f1c17
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-11-06 18:35:15 -0300

    setup home outlet with initial styles

---
+++ app/routes/home.tsx
@@ -0,0 +1,45 @@
+import { Form, NavLink, Outlet } from '@remix-run/react'
+
+export const HomeIcon = () => {
+	return (
+		<svg
+			fill="currentColor"
+			stroke-width="0"
+			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 1024 1024"
+			height="1em"
+			width="1em"
+		>
+			<path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 00-44.4 0L77.5 505a63.9 63.9 0 00-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0018.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
+		</svg>
+	)
+}
+
+export default function HomeOutlet() {
+	return (
+		<>
+			<header className="p-5 ">
+				<nav className="container flex flex-row items-center justify-between max-w-5xl mx-auto ">
+					<h1>
+						<NavLink to="/home">
+							<HomeIcon />
+						</NavLink>
+					</h1>
+					<ul className="flex flex-row items-center space-x-5">
+						<li>
+							<NavLink to="/home/notes">Mis notas</NavLink>
+						</li>
+						<li>
+							<Form action="/logout" method="post">
+								<button type="submit">Cerrar sesión</button>
+							</Form>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main className="h-screen">
+				<Outlet />
+			</main>
+		</>
+	)
+}
