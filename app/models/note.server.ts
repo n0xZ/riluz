f9c42d34269e24fa0ee76d37b8b39a26ef16274b
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-11-06 12:01:19 -0300

    chore: setup note models

---
+++ app/models/note.server.ts
@@ -0,0 +1,26 @@
+import type { Note } from '@prisma/client'
+import { prisma } from '~/utils/prisma.server'
+
+export const getNotesByAuthorId = async (authorId?: string) => {
+	return await prisma.note.findMany({ where: { authorId } })
+}
+
+export const getNoteByNoteId = async (noteId?: string) => {
+	return await prisma.note.findUnique({ where: { id: noteId } })
+}
+
+export const createNote = async ({ title, content, authorId }: Note) => {
+	return await prisma.note.create({
+		data: {
+			title,
+			content,
+			author: { connect: { userId: authorId } },
+		},
+	})
+}
+export const deleteNote = async (noteId?: string) => {
+	return await prisma.note.delete({ where: { id: noteId } })
+}
+export const updateNote = async ({ title, content, id }: Partial<Note>) => {
+	return prisma.note.update({ data: { title, content }, where: { id } })
+}
