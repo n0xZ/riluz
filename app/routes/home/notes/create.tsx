b9e7cbf7b63a5b307664ac7a24bda16e2cc0de5c
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-11-09 15:56:32 -0300

    update: add custom formfield on create notes route

--- app/routes/home/notes/create.tsx
+++ app/routes/home/notes/create.tsx
@@ -1,7 +1,7 @@
 import { Form, useActionData, useTransition } from '@remix-run/react'
 import { z } from 'zod'
-import type { TextareaHTMLAttributes } from 'react'
-import { FormField } from '~/components/form/FormField'
+import type { InputHTMLAttributes, TextareaHTMLAttributes } from 'react'
+
 import {
 	errorMessagesForSchema,
 	inputFromForm,
@@ -17,6 +17,12 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 	name: string
 	errors?: string
 }
+interface FormFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+	label: string
+	type: string
+	name: string
+	errors?: string
+}
 const createNoteSchema = z.object({
 	title: z.string().min(3, { message: 'Campo requerido' }),
 	content: z
@@ -26,13 +32,14 @@ const createNoteSchema = z.object({
 
 export const action = async ({ request }: ActionArgs) => {
 	const userId = await requireUserId(request)
+	console.log(userId)
 	const createNoteFunc = makeDomainFunction(createNoteSchema)(
 		async ({ content, title }) => {
 			return createNote({ title, content, authorId: userId })
 		}
 	)
 	const result = await createNoteFunc(await inputFromForm(request))
-
+	console.log(result)
 	const inputErrors = errorMessagesForSchema(
 		result.inputErrors,
 		createNoteSchema
@@ -53,12 +60,29 @@ export const TextAreaField = ({
 	...rest
 }: TextAreaProps) => {
 	return (
-		<aside className="flex flex-col justify-center max-w-3xl space-y-3">
+		<aside className="flex flex-col justify-center w-full space-y-3">
 			<label className="font-bold ">{label}</label>
 			<textarea
+				name={name}
 				placeholder="El contenido de tu nota a completar"
 				{...rest}
-				className="w-full h-96"
+				className="w-full border-2 outline-none h-96 border-neutral-100"
+			/>
+			<span className="text-red-500 h-9 " data-test="input-errors">
+				{errors && errors}
+			</span>
+		</aside>
+	)
+}
+function FormField({ label, name, type, errors, ...rest }: FormFieldProps) {
+	return (
+		<aside className="flex flex-col justify-center space-y-3">
+			<label className="font-bold ">{label}</label>
+			<input
+				type={type}
+				name={name}
+				className="w-full px-5 py-3 bg-transparent border-2 rounded-lg outline-none border-neutral-100"
+				{...rest}
 			/>
 			<span className="text-red-500 h-9 " data-test="input-errors">
 				{errors && errors}
@@ -93,38 +117,40 @@ export default function CreateNote() {
 	const transition = useTransition()
 	const isSubmitting = transition.state === 'submitting'
 	return (
-		<section className="h-full">
-			<Form method="post">
-				<FormField
-					name="title"
-					type="text"
-					label="Titulo de la nota"
-					errors={actionData && actionData.title && actionData?.title[0]}
-				/>
-				<TextAreaField
-					name="content"
-					placeholder="El contenido de mi nota"
-					label="Contenido de la nota"
-					errors={actionData && actionData.content && actionData?.content[0]}
-				/>
-				<button
-					type="submit"
-					className={`max-w-3xl px-5 py-3 font-bold rounded-lg bg-fuchsia-600 text-gray-50 ${
-						isSubmitting && 'flex flex-row items-center justify-center space-x-3'
-					}`}
-					disabled={isSubmitting}
-					name="submit-login"
-				>
-					{!isSubmitting ? (
-						'Iniciar sesión'
-					) : (
-						<>
-							<LoadingIcon />
-							<span>Iniciando...</span>
-						</>
-					)}
-				</button>
-			</Form>
-		</section>
+		<Form
+			method="post"
+			className="flex flex-col justify-center w-full p-4 space-y-"
+		>
+			<h1 className="mb-3 text-3xl font-bold text-center">Crear nueva nota</h1>
+			<FormField
+				name="title"
+				type="text"
+				label="Titulo de la nota"
+				errors={actionData && actionData.title && actionData?.title[0]}
+			/>
+			<TextAreaField
+				name="content"
+				placeholder="El contenido de mi nota"
+				label="Contenido de la nota"
+				errors={actionData && actionData.content && actionData?.content[0]}
+			/>
+			<button
+				type="submit"
+				className={`w-full px-5 py-3 font-bold rounded-lg bg-fuchsia-600 text-gray-50 ${
+					isSubmitting && 'flex flex-row items-center justify-center space-x-3'
+				}`}
+				disabled={isSubmitting}
+				name="submit-login"
+			>
+				{!isSubmitting ? (
+					'Crear nota'
+				) : (
+					<>
+						<LoadingIcon />
+						<span>Creando...</span>
+					</>
+				)}
+			</button>
+		</Form>
 	)
 }
